ae11508f86c2245c0038e1cf84d81bf2a5aae470
GJZ <597605602@qq.com name = GJZ>
2018-11-06 13:51:41 +0800

    First commit.

---
+++ app/src/main/java/com/example/gjz/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.example.gjz.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceid() {
+        return provinceid;
+    }
+
+    public void setProvinceid(int provinceid) {
+        this.provinceid = provinceid;
+    }
+}
